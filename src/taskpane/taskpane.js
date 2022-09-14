743c0bd571b4ce4541ff49fab2b602dec3956225
Al Arafat Siddique <arafatdipto@gmail.com>
2022-09-14 12:17:02 +0600

    Initial Project

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -8,32 +8,25 @@
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
     // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
-  if (!Office.context.requirements.isSetSupported('ExcelApi', '1.7')) {
-    console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
-  }
-    document.getElementById("run").onclick = run;
+if (!Office.context.requirements.isSetSupported('ExcelApi', '1.7')) {
+  console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
+}
+    document.getElementById("random-number").onclick = generateRandomNumber;
   }
 });
 
-
-
-function run() {
-  Excel.run(function (context) {
-
-    const range = context.workbook.getSelectedRange();
-
-    // Read the range address
-    range.load("address");
-
-    // Update the fill color
-    range.format.fill.color = "yellow";
-
-    return context.sync();
-  })
-  .catch(function (error) {
-    console.log("Error: " + error);
-    if (error instanceof OfficeExtension.Error) {
-      console.log("Debug info: " + JSON.stringify(error.debugInfo));
-    }
-  });
+async function generateRandomNumber() {
+  try {
+    await Excel.run(async (context) => {
+   
+ 
+    await context.sync();
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+
+
+
